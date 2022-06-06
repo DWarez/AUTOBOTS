fb9c5eed65df99ffb0172e144b779600cfc71a6f
DWarez <ldwarezl@gmail.com>
2022-06-06 11:17:59 +0200

    clean up

--- test/utils/data_test.py
+++ test/utils/data_test.py
@@ -3,15 +3,12 @@ sys.path.append('src/utils')
 sys.path.append('src/embeddings')
 
 from data import WikiText2Dataset
-from positional_encoding import PositionalEncoding
-from torch.nn import Embedding
 
 dataset = WikiText2Dataset(128, 35, "cpu")
 
 train, val, test = dataset.get_datasets()
 print(f"Train set shape: {train.shape}")
 
-data, target = dataset.get_batch(train, 10)
+print(f"Vocalubary length: {len(dataset.vocab)}")
 
-print(f"Data shape: {data.shape}")
-print(f"Target shape: {target.shape}")
+print(train[0][:])
